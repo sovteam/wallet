ef13fa9596c8ae4decf2fab2c508f5416a429b15
Adalberto Soares <junynes@gmail.com>
2018-09-09 17:11:43 -0300

    First android commit

---
+++ app/src/main/java/wallet/sov/network/simplestellarwallet/MainActivity.java
@@ -0,0 +1,18 @@
+package wallet.sov.network.simplestellarwallet;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
