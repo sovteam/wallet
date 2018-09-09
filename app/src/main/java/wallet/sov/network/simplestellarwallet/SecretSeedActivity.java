543996850804443d0d00935006d32075d8c9f5a7
Adalberto Soares <junynes@gmail.com>
2018-09-09 18:45:03 -0300

    Adds layout for transactions, send, receive, secret seed and access account

---
+++ app/src/main/java/wallet/sov/network/simplestellarwallet/SecretSeedActivity.java
@@ -0,0 +1,13 @@
+package wallet.sov.network.simplestellarwallet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecretSeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secret_seed);
+    }
+}
